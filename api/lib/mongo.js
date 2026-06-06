6e682fbc0ba1a4ee2e745d97997cab0c6c629a5d
JulianSteckl <steckljn@mail.uc.edu>
2026-06-05 23:53:47 -0400

    Fix MongoDB connection options

--- api/lib/mongo.js
+++ api/lib/mongo.js
@@ -6,8 +6,8 @@ let db = null;
 async function getDb() {
   if (db) return db;
   client = new MongoClient(process.env.MONGODB_URI, {
-    tls: true,
-    tlsAllowInvalidCertificates: false,
+    serverSelectionTimeoutMS: 5000,
+    tlsAllowInvalidCertificates: true,
   });
   await client.connect();
   db = client.db('studenthq');
