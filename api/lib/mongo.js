0a4ff98a2a284bb9e61c5dd1690c3e112eaada72
JulianSteckl <steckljn@mail.uc.edu>
2026-06-05 23:47:34 -0400

    Fix MongoDB TLS connection

--- api/lib/mongo.js
+++ api/lib/mongo.js
@@ -5,7 +5,10 @@ let db = null;
 
 async function getDb() {
   if (db) return db;
-  client = new MongoClient(process.env.MONGODB_URI);
+  client = new MongoClient(process.env.MONGODB_URI, {
+    tls: true,
+    tlsAllowInvalidCertificates: false,
+  });
   await client.connect();
   db = client.db('studenthq');
   return db;
